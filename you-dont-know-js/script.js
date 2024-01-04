87aa9e3f7e6cea5abb484887245366e7c3ef5b1c
Yuriy Kulikov <kulikov-y@ya.ru>
2024-01-04 22:26:43 +0300

    дз к уроку 10-п.1 готово

--- you-dont-know-js/script.js
+++ you-dont-know-js/script.js
@@ -1,2 +1,10 @@
+const lists = document.querySelectorAll('.books')
 const books = document.querySelectorAll('.book')
+console.log(lists)
 console.log(books)
+
+lists[0].prepend(books[1]);
+books[3].replaceWith(books[4]);
+lists[0].append(books[3]);
+lists[0].append(books[5]);
+lists[0].append(books[2]);
