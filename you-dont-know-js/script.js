735e12fcf964012ddd9306028d0f2d6b0008b632
Yuriy Kulikov <kulikov-y@ya.ru>
2024-01-04 22:58:10 +0300

    дз к уроку 10-п.2-готово

--- you-dont-know-js/script.js
+++ you-dont-know-js/script.js
@@ -8,3 +8,6 @@ books[3].replaceWith(books[4]);
 lists[0].append(books[3]);
 lists[0].append(books[5]);
 lists[0].append(books[2]);
+
+const img = document.querySelector('body');
+img.style.backgroundImage = "url(./image/you-dont-know-js.jpg)"
