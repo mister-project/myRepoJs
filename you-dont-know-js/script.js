69e21ad3c0d43086b0fff455efcc07d1d4f3e8b8
Yuriy Kulikov <kulikov-y@ya.ru>
2024-01-05 14:12:05 +0300

    дз к уроку 10-п.4 готово

--- you-dont-know-js/script.js
+++ you-dont-know-js/script.js
@@ -2,7 +2,7 @@ const lists = document.querySelectorAll('.books') //вывод списков
 const books = document.querySelectorAll('.book') //вывод книг одним списком элементов
 const img = document.querySelector('body') // картинка заднего фона
 const titleBooks = document.querySelectorAll('h2') //вывод заголовков книг в список элементов
-
+const advRremove = document.querySelector('.adv')
 
 // п.1 Восстанавливаю порядок книг
 lists[0].prepend(books[1]);
@@ -18,8 +18,11 @@ img.style.backgroundImage = "url(./image/you-dont-know-js.jpg)"
 // п. 3 исправляю заголовок 3-й книги
 titleBooks[4].innerHTML = '<a href="https://github.com/azat-io/you-dont-know-js-ru/blob/master/this%20%26%20object%20prototypes/README.md#you-dont-know-js-this--object-prototypes" target="_blank">Книга 3. this и Прототипы Объектов</a>'
 
+advRremove.classList.remove("adv");
+
 //мусор
 console.log(lists)
 console.log(books)
 console.log(img)
 console.log(titleBooks)
+console.log(advRremove)
