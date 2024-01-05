d59e3cb77a16658b13e407380a861e1b00b60cb6
Yuriy Kulikov <kulikov-y@ya.ru>
2024-01-05 11:04:59 +0300

    дз к уроку 10-п.3 готово+убрался в коде

--- you-dont-know-js/script.js
+++ you-dont-know-js/script.js
@@ -1,22 +1,25 @@
-const lists = document.querySelectorAll('.books')
-const books = document.querySelectorAll('.book')
-const targets = document.querySelectorAll('h2')
-const thirstBook = '<a href="https://github.com/azat-io/you-dont-know-js-ru/blob/master/this%20%26%20object%20prototypes/README.md#you-dont-know-js-this--object-prototypes" target="_blank">Книга 3. this и Прототипы Объектов</a>'
+const lists = document.querySelectorAll('.books') //вывод списков
+const books = document.querySelectorAll('.book') //вывод книг одним списком элементов
+const img = document.querySelector('body') // картинка заднего фона
+const titleBooks = document.querySelectorAll('h2') //вывод заголовков книг в список элементов
 
-console.log(lists)
-console.log(books)
-console.log(targets)
 
+// п.1 Восстанавливаю порядок книг
 lists[0].prepend(books[1]);
 books[3].replaceWith(books[4]);
 lists[0].append(books[3]);
 lists[0].append(books[5]);
 lists[0].append(books[2]);
 
-const img = document.querySelector('body');
+// п.2 меняю картинку заднего фона
+
 img.style.backgroundImage = "url(./image/you-dont-know-js.jpg)"
 
+// п. 3 исправляю заголовок 3-й книги
+titleBooks[4].innerHTML = '<a href="https://github.com/azat-io/you-dont-know-js-ru/blob/master/this%20%26%20object%20prototypes/README.md#you-dont-know-js-this--object-prototypes" target="_blank">Книга 3. this и Прототипы Объектов</a>'
 
-targets[4].innerHTML = '<a href="https://github.com/azat-io/you-dont-know-js-ru/blob/master/this%20%26%20object%20prototypes/README.md#you-dont-know-js-this--object-prototypes" target="_blank">Книга 3. this и Прототипы Объектов</a>'
-
-console.log(thirstBook)
+//мусор
+console.log(lists)
+console.log(books)
+console.log(img)
+console.log(titleBooks)
