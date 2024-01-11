0714386bb8cb8e9b428a590206d5c4c49584189c
Yuriy Kulikov <kulikov-y@ya.ru>
2024-01-11 20:48:54 +0300

    Урок11-финал+

--- listeners/script.js
+++ listeners/script.js
@@ -1,4 +1,4 @@
-//Получение текста из поля
+
 const text = document.getElementById('text')//Получение элемента Input[text]
 const btn = document.getElementById('btn')//Получение элемента button
 const qv = document.getElementById('square')//Получение элемента квадрат из вертстки
@@ -6,31 +6,31 @@ const range = document.getElementById('range')//Получение элемен�
 const span = document.getElementById('range-span')//Получение элемента range-span
 const circle = document.getElementById('circle')
 const eBtn = document.getElementById('e_btn')
+
+
 // Функция для изменения цвета квадрата
 const logger = function (event) { 
- 
-const colorQv = event.target.value;
-
+ const colorQv = event.target.value;
 btn.addEventListener('click', function() {
 qv.style.backgroundColor = colorQv
-
 })
-
 }
+
+
 //Ур 11 - п.2 - Кнопка в круге - style="display: none"
 eBtn.style.display = "none";
+
 // Ниже - функция изменения размеров круга
 const logger1 = function (event) {
     span.textContent = event.target.value;
     
     circle.style.width = event.target.value + '%';
     circle.style.height = event.target.value + '%';
-
     
 }
 
+//Привязка функций к событиям (ниже)
 text.addEventListener('input', logger);//Ур11 п.1 -Исполняемая часть по событию 'input - text'
 range.addEventListener('input', logger1);//Ур11 п.3 - Исполняемая часть по изменению 'input range'
 
-console.log(eBtn.style.display)
-//<button id="e_btn" style="display: none">Жмяк</button>
+
