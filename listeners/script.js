b72a21faf92e36d09d2934ed0cbb11aae40ac4b0
Yuriy Kulikov <kulikov-y@ya.ru>
2024-01-11 19:14:17 +0300

    Урок 11 - сделаны п. 1 и п 3 (без п. 2)

--- listeners/script.js
+++ listeners/script.js
@@ -2,16 +2,32 @@
 const text = document.getElementById('text')//Получение элемента Input[text]
 const btn = document.getElementById('btn')//Получение элемента button
 const qv = document.getElementById('square')//Получение элемента квадрат из вертстки
+const range = document.getElementById('range')//Получение элемента range
+const span = document.getElementById('range-span')//Получение элемента range-span
+const circle = document.getElementById('circle')
 
-
-const logger = function (event) {
+// Функция для изменения цвета квадрата
+const logger = function (event) { 
+ 
 const colorQv = event.target.value;
 
 btn.addEventListener('click', function() {
 qv.style.backgroundColor = colorQv
+
 })
 
 }
+// Ниже - функция изменения размеров круга
+const logger1 = function (event) {
+    span.textContent = event.target.value;
+    
+    circle.style.width = event.target.value + '%';
+    circle.style.height = event.target.value + '%';
 
-text.addEventListener('input', logger);
+    
+}
+
+text.addEventListener('input', logger);//Исполняемая часть по событию 'input - text'
+range.addEventListener('input', logger1);//Исполняемая часть по изменению 'input range'
+
 
