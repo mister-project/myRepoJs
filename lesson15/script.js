8073b2e9f29dd79f6499dd3f9d6b8b4145938fed
Yuriy Kulikov <kulikov-y@ya.ru>
2024-01-28 21:46:49 +0300

    Урок15. Все работает

--- lesson15/script.js
+++ lesson15/script.js
@@ -1,60 +1,46 @@
 'use strict'
 console.log('lesson15')
-// 1) Создать функцию-конструктор DomElement, который
 
-//    содержит свойства
-//   - selector,
-//   - height,
-//   - width,
-//   - bg,
-//   - fontSize
-// содержит метод, который создает элемент на странице в зависимости от условия:
-// - если строка selector начинается с точки, создаем div с классом
-// - если строка selector начинается с решетки # то создаем параграф с id
-// пример:
-// если передана строка '.block', то функция конструктор создает элемент с class="block"
-// если передана строка '#best', то функция конструктор создает элемент с id =best"
-// с помощью cssText задавать стили:
-//   - высотой - height,
-//   - шириной - width,
-//   - background - bg
-//   - размер текста fontSize
-// Внутрь созданного блока записывать любой текст. Метод записи может быть любым.
-
-const DomElement = function () {
+const DomElement = function (selector, height, width, bg, fontSize, text) {
     console.log('DomElement')
-    const selector = '.tr';
-    const height = 100;
-    const width = 100;
-    const bg = prompt('задайте цвет', 'red');
-    const fontSize = 10;
-    const insText = prompt('введите текст', 'ляля');
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.insText = text;
+}
 
-    const crElement = function () {
-        console.log('crElement') //Проверка запуска функции
-        const firstSymb = selector.charAt(0); //Переменная, содержащая первый символ введенного блока
-        const restSymb = selector.slice(1); //Переменная со всеми символами введенного блока, кроме первого
-        let insContent = document.querySelector('.content'); // переменная для вывода результата на страницу (привязываемся к классу 'content')
+DomElement.prototype.crElement = function () {
 
-        //Ниже - цикл для выбора значения переменной insContent + добавления разметки (Вставляемый DIV будет классом или ID )
+    console.log('crElement') //Проверка запуска функции
+    const firstSymb = this.selector.charAt(0); //Переменная, содержащая первый символ введенного блока
+    const restSymb = this.selector.slice(1); //Переменная со всеми символами введенного блока, кроме первого
+    let insContent = document.querySelector('.content'); // переменная для вывода результата на страницу (привязываемся к классу 'content')
 
-        if (firstSymb === '.') {
-            insContent.innerHTML = `<div class="` + restSymb + `">Ваш текст: "` + insText + `", а DIV с классом ` + restSymb + ` </div>`
+    //Ниже - цикл для выбора значения переменной insContent + добавления разметки (Вставляемый DIV будет классом или ID )
 
-        } else if (firstSymb === '#') {
-            insContent.innerHTML = `<div id="` + restSymb + `">Ваш текст: ` + insText + `, а DIV  c Идентификатором  ` + restSymb + `</div>`
-        }
+    if (firstSymb === '.') {
+        insContent.innerHTML = `<div class="` + restSymb + `">Ваш текст: "` + this.insText + `", а DIV с классом ` + restSymb + ` </div>`
 
-        //Ниже дополнительная переменная для выделения DIV из результата цикла и добавления новых стилей CSS
-        const insStyle = insContent.querySelector('div') //
-
-
-        //Ниже задаю остальные свойства блока DIV с использованием CssText
-        insStyle.style.cssText = 'height: ' + height + 'px; width: ' + width + 'px; background: ' + bg + '; font-size: ' + fontSize + 'px;'
-
+    } else if (firstSymb === '#') {
+        insContent.innerHTML = `<div id="` + restSymb + `">Ваш текст: ` + this.insText + `, а DIV  c Идентификатором  ` + restSymb + `</div>`
     }
-    crElement()
+
+    //Ниже дополнительная переменная для выделения DIV из результата цикла и добавления новых стилей CSS
+    const insStyle = insContent.querySelector('div') //
+
+
+    //Ниже задаю остальные свойства блока DIV с использованием CssText
+    insStyle.style.cssText = 'height: ' + this.height + 'px; width: ' + this.width + 'px; background: ' + this.bg + '; font-size: ' + this.fontSize + 'px;'
 
 }
 
-DomElement()
+
+
+
+const newObject = new DomElement('.block', 50, 50, 'green', 15, '123')
+console.log(newObject)
+newObject.crElement();
+
+console.log(newObject)
