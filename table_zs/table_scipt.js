f37a4f6d04008956d901b37989ab50673ffcfbbe
Yuriy Kulikov <kulikov-y@ya.ru>
2024-02-02 13:13:24 +0300

    Табл-ЗС-работает расчет уникальных классов

--- table_zs/table_scipt.js
+++ table_zs/table_scipt.js
@@ -15,37 +15,37 @@ const elements = document.querySelectorAll('[class*="ant"]');
 console.log(elements);
 
 //Шаг2. Надо преобразовать nodeList в массив 
-const array = ['tdant1', 'tdant1', 'td.ant2', 'td.ant3', 'td.ant3', 'td.ant3', 'td.ant4', 'td.ant4'];
-elements.forEach((element) => {
-    console.log(element)
-    array.push(element);
+const array = [];
+elements.forEach((y) => {
+    console.log(y.className);
+
+    array.push(y.className); // сама публикация новых элементов
+    console.log(array);
 });
-console.log(array);
+
 //Шаг3. Удаляем из массива дубли
-const uniqArray = new Set(array)
-console.table(uniqArray)
-// let debug = [];
-// const uniqArray = array.filter((item, index) => {
-//     debug.push({
-//         item,
-//         index,
-//         indexOf: array.indexOf(item)
-//     })
-//     return index === array.indexOf(item);
-// });
-
-// console.table(debug)
 
 
-// for (let j = 1; j < 5; j++) {
-//     let cl = "ant" + j;
-//     console.log(cl);
-//     const elements1 = document.getElementsByClassName(cl);
-//     // elements[i].remove();
-//     //console.log(elements1.length);
+const uniqArray = new Set(array);
+console.log(uniqArray);
+
+//Конец щага3
+//Получаем количество уникальных классов (станций)
+const lengthArr = uniqArray.size
+console.log(lengthArr)
 
 
-// }
+
+for (let j = 1; j < lengthArr + 1; j++) {
+    let cl = "ant" + j;
+    console.log(cl);
+    const elements1 = document.getElementsByClassName(cl);
+    // elements[i].remove();
+    console.log(elements1);
+    console.log(elements1.length);
+
+
+}
 
 
 // var arr = [1, 2, 2, 3, 4, 5, 5, 5, 6, 7, 7, 8, 9, 10, 10];
