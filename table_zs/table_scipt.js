42473e832f40ecab72cce3c9ad5e36bbb2251c3b
Yuriy Kulikov <kulikov-y@yandex.ru>
2024-02-04 00:17:34 +0000

    Таблица - вроде все работает

--- table_zs/table_scipt.js
+++ table_zs/table_scipt.js
@@ -1,13 +1,6 @@
 'use srtict'
 
-// const element = document.querySelectorAll('tr.td');
-// console.log(this.element);
-// const element1 = document.querySelectorAll('table')
-// console.log(element1);
-// let myTable = document.querySelector("#table");
-// let allTds = myTable.querySelectorAll("td");
-// let allTrs = myTable.querySelectorAll("tr");
-// console.log(myTable);
+
 
 //считаем количество антенн
 //Шаг1. выводим все записи чтобы найти дубликаты
@@ -17,10 +10,10 @@ console.log(elements);
 //Шаг2. Надо преобразовать nodeList в массив 
 const array = [];
 elements.forEach((y) => {
-    console.log(y.className);
+    
 
     array.push(y.className); // сама публикация новых элементов
-    console.log(array);
+    
 });
 
 //Шаг3. Удаляем из массива дубли
@@ -34,33 +27,34 @@ console.log(uniqArray);
 const lengthArr = uniqArray.size
 console.log(lengthArr)
 
-
+//Цикл для перебора станций и создания отдельных коллекций по каждому классу
 
 for (let j = 1; j < lengthArr + 1; j++) {
     let cl = "ant" + j;
     console.log(cl);
-    const elements1 = document.getElementsByClassName(cl);
+    let elements1 = document.querySelectorAll(`.${cl}`);
+    //Определение количества одинаковых ячеек (строк по каждой станции)
     let elemLen = elements1.length;
+    
+    const array1 = Array.from(elements1);
+   
+   
     for (let k = 0; k < elemLen; k++) {
-        console.log(elements1[k])
+        console.log(array1[k]);
+        if(k === 0) {
+            array1[k].setAttribute('rowspan', `${elemLen}`);
+
+        } else {
+            array1[k].remove();
+        }
+     
     }
+   
+} 
 
-}
-
-
-// var arr = [1, 2, 2, 3, 4, 5, 5, 5, 6, 7, 7, 8, 9, 10, 10];
-
-// function squash(arr) {
-//     var tmp = [];
-//     for (var i = 0; i < arr.length; i++) {
-//         if (tmp.indexOf(arr[i]) == -1) {
-//             tmp.push(arr[i]);
-//         }
-//     }
-//     return tmp;
-// }
-
-// console.log(squash(arr));
+    
 
 
-// console.log(element2.length);
+
+
+
