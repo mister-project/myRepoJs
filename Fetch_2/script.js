7e2586ea6f3c3e83f4ae906e390f00caa7a7d0c0
Yuriy Kulikov <kulikov-y@ya.ru>
2024-03-16 22:43:58 +0300

    ДЗ

---
+++ Fetch_2/script.js
@@ -0,0 +1,26 @@
+const urlGet = "db.json";
+const urlSend = "https://jsonplaceholder.typicode.com/posts";
+
+const getData = (url) => {
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
+};
+
+const sendData = (url, data) => {
+  return fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
+};
+
+
+
+sendData(urlSend, getData(urlGet))
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
